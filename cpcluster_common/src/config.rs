0972c353d2f55a18eeab38cd23f75bdcde7cf4f4
Jonas Zielke <jonas.c.zielke@gmail.com>
2025-06-12 16:11:56 +0200

    Add TLS certificate verification

--- cpcluster_common/src/config.rs
+++ cpcluster_common/src/config.rs
@@ -8,6 +8,7 @@ pub struct Config {
     pub failover_timeout_ms: u64,
     pub master_addresses: Vec<String>,
     pub ca_cert_path: Option<String>,
+    pub ca_cert: Option<String>,
     pub cert_path: Option<String>,
     pub key_path: Option<String>,
 }
@@ -20,6 +21,7 @@ impl Default for Config {
             failover_timeout_ms: 5000,
             master_addresses: vec!["127.0.0.1:55000".to_string()],
             ca_cert_path: None,
+            ca_cert: None,
             cert_path: None,
             key_path: None,
         }
